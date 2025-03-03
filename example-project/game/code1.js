6f629769dea00ceece8a2d9dd3737ac269bde524
Gutsul228 <273116viv@gmail.com>
2025-03-03 13:35:50 +0000

    123

---
+++ example-project/game/code1.js
@@ -0,0 +1,20 @@
+gdjs.GamoverCode = {};
+gdjs.GamoverCode.localVariables = [];
+
+
+gdjs.GamoverCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.GamoverCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.GamoverCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['GamoverCode'] = gdjs.GamoverCode;
